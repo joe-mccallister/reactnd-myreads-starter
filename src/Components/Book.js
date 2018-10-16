7a07873cdfee759986538b0de02ac6d64901ddb4
Joe McCallister <macattack1380@gmail.com>
2018-10-16 12:35:08 -0600

    merge Book.js into new repo

---
+++ src/Components/Book.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Book extends Component {
+    static PropTypes = {
+        book: PropTypes.object.isRequired,
+        onUpdateShelf: PropTypes.func.isRequired
+    }
+    render() {
+        return (
+            <li>
+                <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div>
+                    <div className="book-shelf-changer">
+                    <select
+                        onChange={(e) => {
+                            this.props.onUpdateShelf(this.props.book, e.target.value)
+                        }}
+                        value={this.props.book.shelf}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    
+                    </select>
+                    </div>
+                </div>
+                <div className="book-title">{this.props.book.title}</div>
+                <div className="book-authors">{this.props.book.authors && this.props.book.map(author => author).filter(authors => authors.length > 1)}</div>
+                </div>
+            </li>
+        )
+    }
+}
